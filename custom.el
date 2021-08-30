f72b30c83478d6a046be458cd56881d10d5473c9
el-sapo <ericsapiens@iconbuild.com>
2021-08-30 14:23:24 -0500

    Third attempt 20210830

---
+++ custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(c-noise-macro-names '("constexpr"))
+ '(package-selected-packages
+   '(mwim drag-stuff iedit expand-region ace-window sql-indent sqlup-mode openwith smartparens powerline doom-themes auctex lua-mode yasnippet-snippets flycheck-rust rust-mode json-mode yaml-mode cmake-mode gitignore-mode git-timemachine diff-hl forge magit google-c-style cuda-mode smart-hungry-delete autopair web-mode vlf org-superstar writegood-mode multiple-cursors string-inflection flycheck-pyflakes flycheck company-box company-prescient lsp-ui modern-cpp-font-lock clang-format yapfify elpy realgud visual-regexp-steroids undo-tree zzz-to-char avy which-key beacon rainbow-delimiters origami edit-server rg wgrep winum ggtags lsp-ivy flyspell-correct-ivy counsel-etags counsel-projectile counsel swiper ivy-prescient ivy use-package-hydra hydra all-the-icons ws-butler diminish auto-package-update async esup benchmark-init use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background "black"))))
+ '(avy-lead-face ((t (:foreground "black" :background "#c678dd" :bold t :underline t))))
+ '(company-tooltip ((t (:background nil))))
+ '(company-tooltip-selection ((t (:background nil :underline t))))
+ '(diff-hl-change ((t (:background "#5f00af" :foreground "#5f00af"))))
+ '(highlight ((t (:background "nil" :foreground "nil" :underline t))))
+ '(ivy-current-match ((t (:background nil :underline t))))
+ '(powerline-active1 ((t (:background "black" :foreground "#c678dd" :bold t :underline t))))
+ '(powerline-active2 ((t (:background "black" :foreground "#c678dd" :bold t :underline t))))
+ '(which-func ((t (:foreground "blue")))))
